8ce957c3e7831b232d71813eb8821d99970dfc12
ejaszczuk <emj1054@gmail.com>
2021-05-17 22:15:00 +0200

    Fix run.py

--- run.py
+++ run.py
@@ -2,6 +2,17 @@ from app import create_app
 from scheduler import scheduler
 import sys
 
+# if __name__ == '__main__':
+#     app = create_app('config.DevelopmentConfig')
+
+#     if len(sys.argv) > 1:
+#         if sys.argv[1] == 'prod':
+#             app = create_app('config.ProductionConfig')
+
+#     scheduler.init_app(app)
+#     scheduler.start()
+#     app.run()
+
 app = create_app('config.DevelopmentConfig')
 
 if len(sys.argv) > 1:
