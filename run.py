d58534c1241900a68496dc1186b7451191899f31
ejaszczuk <emj1054@gmail.com>
2021-05-17 22:21:44 +0200

    ehhgghhh

--- run.py
+++ run.py
@@ -2,23 +2,23 @@ from app import create_app
 from scheduler import scheduler
 import sys
 
-# if __name__ == '__main__':
-#     app = create_app('config.DevelopmentConfig')
+if __name__ == '__main__':
+    app = create_app('config.DevelopmentConfig')
 
-#     if len(sys.argv) > 1:
-#         if sys.argv[1] == 'prod':
-#             app = create_app('config.ProductionConfig')
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'prod':
+            app = create_app('config.ProductionConfig')
 
-#     scheduler.init_app(app)
-#     scheduler.start()
-#     app.run()
+    scheduler.init_app(app)
+    scheduler.start()
+    app.run()
 
-app = create_app('config.DevelopmentConfig')
+# app = create_app('config.DevelopmentConfig')
 
-if len(sys.argv) > 1:
-    if sys.argv[1] == 'prod':
-        app = create_app('config.ProductionConfig')
+# if len(sys.argv) > 1:
+#     if sys.argv[1] == 'prod':
+#         app = create_app('config.ProductionConfig')
 
-scheduler.init_app(app)
-scheduler.start()
-app.run()
+# scheduler.init_app(app)
+# scheduler.start()
+# app.run()
